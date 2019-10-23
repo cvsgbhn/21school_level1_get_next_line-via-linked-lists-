cab6cc6d7b61973a971f8ccc5a40738ff409f264
cvsgbhn <kbh123456g@gmail.com>
2019-10-23 23:34:26 +0300

    Just committing so I can go home

--- get_next_line.c
+++ get_next_line.c
@@ -15,6 +15,32 @@ t_list	ft_check_fd(int intro_fd, char **line, t_list **text_list)
 	return (text_list);
 }
 
+char    *ft_check_endings(char **line, t_list *text_list)
+{
+  char    *ptr_to_end;
+  size_t   size_of_content;
+
+  ptr_to_end = NULL;
+  size_of_content = ft_strlen(text_list->content);
+  if (text_list->content)
+  {
+    if ((ptr_to_end = ft_strchr(text_list->content, '\n')))
+    {
+      *ptr_to_end = '\0';
+      *line = ft_strdup(text_list->content);
+      ft_strcpy(text_list->content, ++ptr_to_end);
+    }
+    else
+    {
+      *line = ft_strdup(text_list->content);
+      //ft_lstdelone(text_list, size_of_content);   ?????howusethisfuckingshit
+    }
+  }
+  else
+    *line =   ft_strnew(1);
+  return (ptr_to_end);
+}
+
 int		get_next_line(const int fd, char **line)
 {
 	static t_list		*text_list;
@@ -25,6 +51,7 @@ int		get_next_line(const int fd, char **line)
 
 	if (read(fd, NULL, 0) == -1)
 	  return (-1);
+	ptr_to_end = ft_check_endings(fd, line);
 	text_list = ft_check_fd(fd, line, text_list);
 	while (!ptr_to_end && (num_read = read(fd, buf, BUFF_SIZE)))
         {
